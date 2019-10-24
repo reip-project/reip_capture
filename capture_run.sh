fcde7e4ee3c7d0b86b1ec435658c29ea8bb4cad3
cmydlarz <cmydlarz>
2019-10-24 16:40:03 -0400

    double quotres around crontab

--- capture_run.sh
+++ capture_run.sh
@@ -10,7 +10,7 @@ task_dir=$local_dir/capture
 local_data_dir="/mnt/reip_data"
 
 # Add nightly shutdown cronjob
-crontab -l | grep -q 'nightly_reboot'  && echo 'entry exists' || cat <(crontab -l) <(echo '0 2 * * * $local_dir/setup/nightly_reboot.sh') | crontab -
+crontab -l | grep -q 'nightly_reboot'  && echo 'entry exists' || cat <(crontab -l) <(echo "0 2 * * * $local_dir/setup/nightly_reboot.sh") | crontab -
 
 find $local_dir -name "*.sh" -exec chmod +x {} +
 chown -R reip $local_dir
